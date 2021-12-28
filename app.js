52c625a16e8760627920799ca1bfae38953ca04c
Leslie-Rae <84047891+Leslie-Rae@users.noreply.github.com>
2021-12-28 15:28:52 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,173 @@
+
+
+// const PALETTE = [
+//   'red',
+//   'orange',
+//   'yellow',
+//   'green',
+//   'blue',
+//   'indigo',
+//   'violet',
+//   'pink',
+//   'turquoise',
+// ]
+
+// function makePalette() {
+//   for (let index = 0; index < PALETTE.length; index = index + 1) {
+//     const nextColor = PALETTE[index]
+//     let newButton = $('<button>' + '</button>')
+//     $(newButton).css('background-color', nextColor)
+//     $('.palette').append(newButton)
+//     $('.palette button').first().addClass('active');
+//   }
+// }
+// makePalette()
+
+
+// function makeGrid() {
+//   for (let i = 0; i < 64; i++) {
+//     let newDiv = $('<div class="cell"></div>');
+//     $('.grid').append(newDiv);
+//   }
+// }
+// makeGrid()
+
+// function onPaletteClick() {
+//   $('.active').removeClass()
+//   $(this).addClass('active')
+// }
+// $('.palette button').click(onPaletteClick);
+
+
+// function onGridClick() {
+//   let activeColor = $('.palette .active').css("background-color");
+//   let cellColor = $(this).css('background-color');
+//   if (cellColor !== activeColor) {
+//     $(this).css('background-color', activeColor)
+//   } else {
+//     $(this).css('background-color', "")
+//   }
+// }
+
+// $('.grid .cell').click(onGridClick);
+
+
+// function onClearClick() {
+//   $('.grid .cell').css('background-color', '');
+// }
+
+// $('.controls .clear').click(onClearClick);
+
+
+// function onFillAllClick() {
+//   let activeColor = $('.palette .active').css("background-color");
+//   $('.cell').css('background-color', activeColor);
+// }
+
+// $('.controls .fill-all').click(onFillAllClick);
+
+
+// function onFillEmptyClick() {
+//   const elements = $('.cell');
+//   let activeColor = $('.palette .active').css("background-color");
+//   for (let i = 0; i < elements.length; i++) {
+//     let nextElement = $(elements[i]);
+//     if (nextElement.css('background-color') === 'rgba(0, 0, 0, 0)') {
+//       $(nextElement).css('background-color', activeColor);
+//     }
+
+//   }
+// }
+// $('.controls .fill-empty').click(onFillEmptyClick)
+
+
+//Create the palette of colors to choose from
+makePalette = () => {
+
+  const PALETTE = [
+    '#f94144',
+    '#f3722c',
+    '#f8961e',
+    '#f9c74f',
+    '#90be6d',
+    '#43aa8b',
+    '#277da1',
+    '#5e60ce'
+  ]
+  for (let i = 0; i < PALETTE.length; i++) {
+    const nextColor = PALETTE[i];
+    let newButton = $('<button>' + '</button>')
+    $(newButton).css('background-color', nextColor)
+    $('.palette').append(newButton)
+  }
+  $('.palette button').first().addClass('active');
+}
+makePalette()
+
+
+
+//create and add cells to the .grid using a for loop
+makeGrid = () => {
+  for (let i = 0; i < 64; i++) {
+    $('.grid').append('<div class="cell"></div>')
+    console.log(i)
+  }
+}
+makeGrid()
+
+//selects an active color from the palette
+function onPaletteClick() {
+  $('.active').removeClass()
+  $(this).addClass('active')
+}
+$('.palette button').click(onPaletteClick)
+
+//applies the active color to the selected grid cell, alternatively, removes the color when clicked twice
+function onGridClick() {
+  const activeColor = $('.active').css('background-color')
+
+  if ($(this).css('background-color') === activeColor) {
+    $(this).css('background-color', 'rgba(0, 0, 0, 0)')
+  } else {
+    $(this).css('background-color', activeColor)
+  }
+}
+$('.grid .cell').click(onGridClick)
+
+//clears all grid cells of color
+function onClearClick() {
+  $('.cell').css('background-color', "")
+}
+$('.controls .clear').click(onClearClick)
+
+//fills all grid cells withh active color
+function onFillAllClick() {
+  let activeColor = $('.active').css('background-color')
+  $('.cell').css('background-color', activeColor)
+}
+$('.controls .fill-all').click(onFillAllClick)
+
+//checks to see if cell already has a color and applies active color to only the empty ones
+function onFillEmptyClick() {
+  let activeColor = $('.active').css('background-color')
+  const elements = $('.cell')
+
+  for (let i = 0; i < elements.length; i++) {
+    let nextElement = $(elements[i]);
+    if (nextElement.css('background-color') === 'rgba(0, 0, 0, 0)') {
+      $(nextElement).css('background-color', activeColor)
+    }
+
+  }
+}
+$('.controls .fill-empty').click(onFillEmptyClick)
+
+//allows
+function cursorPaint() {
+  $(".cell").mouseover(function () {
+    const activeColor = $('.active').css('background-color')
+    $(this).css({ background: activeColor });
+  });
+}
+$('.controls .paint').click(cursorPaint)
+
